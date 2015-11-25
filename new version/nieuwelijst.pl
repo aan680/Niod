2a539d39102767bf7e2efb8904be0b19317484a3
Victor <v.de.boer@vu.nl>
2015-11-25 13:18:47 +0100

    added more new terms and hierarchy

---
+++ new version/nieuwelijst.pl
@@ -0,0 +1,114 @@
+bla('Eerste wereldoorlog', 'Vooroorlogse periode').
+bla('Interbellum', 'Vooroorlogse periode').
+bla('Mobilisatie', 'Oorlogvoering').
+bla('Meidagen 1940', 'Oorlogvoering').
+bla('Bombardementen', 'Oorlogvoering').
+bla('Militaire operaties', 'Oorlogvoering').
+bla('Geallieerden', 'Oorlogvoering').
+bla('As-mogendheden', 'Oorlogvoering').
+bla('Capitulatie', 'Oorlogvoering').
+bla('Marine', 'Strijdkrachten').
+bla('Luchtmacht', 'Strijdkrachten').
+bla('Landmacht', 'Strijdkrachten').
+bla('Nederlands bestuur', 'Bestuur').
+bla('Regering in ballingschap', 'Bestuur').
+bla('Bezettingsbestuur', 'Bestuur').
+bla('Fascisme', 'Ideologieën').
+bla('Nationaal-Socialisme', 'Ideologieën').
+bla('Communisme', 'Ideologieën').
+bla('Luchtbescherming', 'Openbare orde').
+bla('Politie', 'Openbare orde').
+bla('Brandweer', 'Openbare orde').
+bla('Voedselvoorziening', 'Dagelijks leven').
+bla('Hongerwinter', 'Dagelijks leven').
+bla('Distributie', 'Dagelijks leven').
+bla('Huishoudens', 'Dagelijks leven').
+bla('Vrijheidsinperkingen', 'Dagelijks leven').
+bla('Sport', 'Dagelijks leven').
+bla('Feestdagen', 'Dagelijks leven').
+bla('Kinderen', 'Dagelijks leven').
+bla('Bevolkingsgroepen', 'Dagelijks leven').
+bla('Gezondheidszorg', 'Dagelijks leven').
+bla('Recreatie', 'Dagelijks leven').
+bla('Hulpverlening', 'Dagelijks leven').
+bla('Arbeid', 'Economie').
+bla('Industriën', 'Economie').
+bla('Handel', 'Economie').
+bla('Transport', 'Economie').
+bla('Landbouw', 'Economie').
+bla('Bankwezen', 'Economie').
+bla('Posterijen', 'Economie').
+bla('Telegrafie', 'Economie').
+bla('Telefonie', 'Economie').
+bla('Arbeidsinzet', 'Tewerkstelling').
+bla('Dwangarbeid', 'Tewerkstelling').
+bla('Arbeidsdienst', 'Tewerkstelling').
+bla('Wetenschappen', 'Onderwijs').
+bla('Gewapend verzet', 'Verzet').
+bla('Crossings', 'Verzet').
+bla('Droppings', 'Verzet').
+bla('Pilotenhulp', 'Verzet').
+bla('Stakingen', 'Verzet').
+bla('Illegale pers', 'Verzet').
+bla('Onderduik', 'Verzet').
+bla('Verzetsgroepen', 'Verzet').
+bla('Engelandvaarders', 'Verzet').
+bla('Anti-Duitse houding', 'Verzet').
+bla('Represailles', 'Verzet').
+bla('NSB', 'Collaboratie').
+bla('Antisemitisme', 'Vervolging').
+bla('Jodenvervolging', 'Vervolging').
+bla('Politieke gevangenen', 'Vervolging').
+bla('Gehandicapten', 'Vervolging').
+bla('Sinti en Roma', 'Vervolging').
+bla('Jehova\'s Getuigen', 'Vervolging').
+bla('Homoseksuelen', 'Vervolging').
+bla('Asocialen', 'Vervolging').
+bla('Gevangenissen', 'Kampen').
+bla('Krijgsgevangenenkampen', 'Kampen').
+bla('Gijzelaarskampen', 'Kampen').
+bla('Concentratiekampen', 'Kampen').
+bla('Doorgangskampen', 'Kampen').
+bla('Vernietigingskampen', 'Kampen').
+bla('Interneringskampen', 'Kampen').
+bla('Buitenkampers', 'Kampen').
+bla('Intochten', 'Bevrijding').
+bla('Aftocht', 'Bevrijding').
+bla('Feesten', 'Bevrijding').
+bla('Bijltjesdag', 'Bevrijding').
+bla('Bevrijdingskinderen', 'Bevrijding').
+bla('Geallieerde bezetting', 'Bevrijding').
+bla('Bijzondere Rechtspraak', 'Naoorlogse periode').
+bla('Militaire rechtspraak', 'Naoorlogse periode').
+bla('Militair Gezag', 'Naoorlogse periode').
+bla('Rechtsherstel', 'Naoorlogse periode').
+bla('Repatriering', 'Naoorlogse periode').
+bla('Zuivering', 'Naoorlogse periode').
+bla('Wederopbouw', 'Naoorlogse periode').
+bla('Bersiap', 'Naoorlogse periode').
+bla('Politionele acties', 'Naoorlogse periode').
+bla('Souvereiniteitsoverdracht', 'Naoorlogse periode').
+bla('Oorlogsmisdadigers', 'Naoorlogse periode').
+bla('Indische Nederlanders', 'Naoorlogse periode').
+bla('Herinnering', 'Naoorlogse periode').
+bla('Verwerking', 'Naoorlogse periode').
+
+
+
+
+
+new('http://data.niod.nl/concept/1501','Meidagen 1940').
+new('http://data.niod.nl/concept/1502','Nederlands bestuur').
+new('http://data.niod.nl/concept/1503','Regering in ballingschap').
+new('http://data.niod.nl/concept/1504','Bezettingsbestuur').
+new('http://data.niod.nl/concept/1505','Vrijheidsinperkingen').
+new('http://data.niod.nl/concept/1506','NSB').
+new('http://data.niod.nl/concept/1507','Geallieerde bezetting').
+new('http://data.niod.nl/concept/1508','Militaire rechtspraak').
+new('http://data.niod.nl/concept/1509','Bersiap').
+new('http://data.niod.nl/concept/1510','Indische Nederlanders').
+
+
+
+
+
